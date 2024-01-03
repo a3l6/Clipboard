278875f014652895b2bd8234f4d25bcfa4851b8d
Emilio Mendoza <ironislife39@gmail.com>
2024-01-03 13:33:20 -0500

    Added load from html file

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import webview
 
-webview.create_window('Woah dude!', html='<h1>Woah dude!<h1>')
+
+def load_html_as_str(path: str) -> str:
+    with open(path, "r") as f:
+        content = f.read()
+    return content
+
+
+webview.create_window(title="", html=load_html_as_str("web/index.html"), width=300, height=400, resizable=False)
 webview.start()
